7d69a7b2e3f5bb02e66ba59af91532d22ed61b64
ole4ka140592 <ole4ka140592@gmail.com>
2021-12-13 18:57:52 +0300

    54, add componentDidMount in UsersCl

--- src/components/Users/UsersCl.tsx
+++ src/components/Users/UsersCl.tsx
@@ -8,15 +8,11 @@ type UsersPropsType = MapStateToPropsType & MapDispatchToPropsType
 
 export class UsersCl extends React.Component<UsersPropsType> {
 
-    constructor(props: UsersPropsType) {
-        super(props);
-
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items)
         })
-
     }
-
 
     render() {
         return (
