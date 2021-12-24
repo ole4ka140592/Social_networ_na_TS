8ec9e1fc7b7f5e5b3a96b42c1a3ffc18ae7a4c73
ole4ka140592 <ole4ka140592@gmail.com>
2021-12-24 22:48:10 +0300

    61, 1) add HeaderContainer 2) changes were made to Арр

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Header = () => {
     return (
@@ -7,7 +8,10 @@ export const Header = () => {
             <img
                 src='https://img2.freepng.ru/20180304/lpw/kisspng-airplane-flight-logo-clip-art-space-shuttle-5a9b9a1d9ee683.3404936215201469736509.jpg'/>
 
+            <div className={classes.loginBlock}>
+                <NavLink to={"/login"}>Login</NavLink>
 
+            </div>
         </header>
     )
 }
