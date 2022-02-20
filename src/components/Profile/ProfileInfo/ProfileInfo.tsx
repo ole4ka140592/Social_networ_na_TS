ceb674f210e71fbcebc6b25335d64a43ae4e0015
ole4ka140592 <ole4ka140592@gmail.com>
2022-02-20 23:49:17 +0300

    74, componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -29,7 +29,9 @@ export const ProfileInfo = (props: ProfileInfoPropsType) => {
 
                     {props.profile.aboutMe}
 
+
                 <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+
 
 
             </div>
--- src/components/Profile/ProfileStatus.tsx
+++ src/components/Profile/ProfileStatus.tsx
@@ -1,74 +1,112 @@
-import React, {ChangeEvent, useState} from "react";
-// export class ProfileStatus extends React.Component {
-//
-//     state = {
-//         editMode: false
-//     }
-//
-//      editModeTrue = () => {
-//         alert("включен")
-//     }
-//
-//     render() {
-//         return (
-//             <div>
-//                 {!this.state.editMode &&
-//                 <div>
-//                     <span onClick={this.editModeTrue}>{this.props.status}</span>
-//                 </div>
-//                 }
-//                 {this.state.editMode &&
-//                 <div>
-//                     <input value={this.props.status}/>
-//                 </div>
-//                 }
-//             </div>
-//         )
-//     }
-// }
+import React, {ChangeEvent} from "react";
+
 
 type ProfileStatusPropsType = {
     status: string
-    updateStatus: (status: string)=> void
+    updateStatus: (status: string) => void
 }
 
-export const ProfileStatus = (props: ProfileStatusPropsType) => {
 
-    let [input, setInput] = useState(props.status)
-    let [editMode, setEditMode] = useState(false)
+export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
 
-    const editModeTrue = () => {
-        setEditMode(true)
+    state = {
+        editMode: false,
+        status: this.props.status
     }
 
-    const editModeFalse = (e: ChangeEvent<HTMLInputElement>) => {
-        setEditMode(false)
-        props.updateStatus(e.currentTarget.value)
+     editModeTrue = () => {
+        this.setState({
+            editMode: true
+        })
     }
 
-    const onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => {
-        setInput(e.currentTarget.value)
+    editModeFalse = () => {
+        this.setState({
+            editMode: false
+        })
+        this.props.updateStatus(this.state.status)
     }
 
+    onStatusChange = (e: ChangeEvent<HTMLInputElement>)=> {
+        this.setState({
+            status: e.currentTarget.value
+        })
+    }
 
-    return (
-        <div>
+    componentDidUpdate(prevProps: Readonly<{status: string}>, prevState: Readonly<{}>) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
+    render() {
+        return (
             <div>
-                {!editMode &&
+                {!this.state.editMode &&
                 <div>
-                    <span onClick={editModeTrue}>{props.status || "No status"}</span>
+                    <span onClick={this.editModeTrue}>{this.props.status || "-----"}</span>
                 </div>
                 }
-                {editMode &&
+                {this.state.editMode &&
                 <div>
-                    <input onBlur={editModeFalse}
-                           onChange={onChangeStatus}
-                           value={input}
-                           autoFocus={true}
-                    />
+                    <input value={this.state.status}
+                           onBlur={this.editModeFalse}
+                           onChange={this.onStatusChange}
+                           autoFocus={true}/>
                 </div>
                 }
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+// type ProfileStatusPropsType = {
+//     status: string
+//     updateStatus: (status: string)=> void
+// }
+
+// export const ProfileStatus = (props: ProfileStatusPropsType) => {
+//
+//     let [input, setInput] = useState(props.status)
+//     let [editMode, setEditMode] = useState(false)
+//
+//     const editModeTrue = () => {
+//         setEditMode(true)
+//     }
+//
+//     const editModeFalse = (e: ChangeEvent<HTMLInputElement>) => {
+//         setEditMode(false)
+//         props.updateStatus(e.currentTarget.value)
+//     }
+//
+//     const onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => {
+//         debugger
+//         if (input !== props.status) {
+//             setInput(e.currentTarget.value)
+//         }
+//     }
+
+
+//     return (
+//         <div>
+//             <div>
+//                 {!editMode &&
+//                 <div>
+//                     <span onClick={editModeTrue}>{props.status || "No status"}</span>
+//                 </div>
+//                 }
+//                 {editMode &&
+//                 <div>
+//                     <input onBlur={editModeFalse}
+//                            onChange={onChangeStatus}
+//                            value={input}
+//                            autoFocus={true}
+//                     />
+//                 </div>
+//                 }
+//             </div>
+//         </div>
+//     )
+// }
