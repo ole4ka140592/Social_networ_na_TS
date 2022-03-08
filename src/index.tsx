b08d6ee90d6a8330a055b6e7f662776f8c0b5c02
ole4ka140592 <ole4ka140592@gmail.com>
2022-03-08 23:29:25 +0300

    95

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./redux/reduxStore";
 import App from "./App";
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter >
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>, document.getElementById('root')
+    </HashRouter>, document.getElementById('root')
 );
 
 
