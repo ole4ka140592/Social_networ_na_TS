e18f6fc730496db44d6eef05dd82019a461afd40
ole4ka140592 <ole4ka140592@gmail.com>
2022-03-31 00:12:12 +0300

    changes in css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import classes from './Dialogs.module.css'
 import {Message} from "./Message/Message";
 import {DialogItem} from "./DialogItem/DialogItem";
-import {DialogsPageStateType} from "../../redux/dialogsReducer";
 import {Redirect} from "react-router-dom";
 import {reduxForm, Field, InjectedFormProps} from "redux-form";
 import {Textarea} from "../common/FormsControls/FormControls";
 import {maxLengthCreator, required} from "../../utils/validators/validators";
 import {OwnPropsType} from "./DialogsContainer";
-import {Preloader} from "../common/Preloader/Preloader";
-import {TextField} from "@material-ui/core";
+import {Divider, List, ListItem, ListItemText} from "@material-ui/core";
 
 
 // type PropsDialogsType = {
@@ -33,20 +31,45 @@ export const Dialogs = (props: OwnPropsType) => {
         return <Redirect to={"/login"}/>
     }
 
+    const style = {
+        width: '250px',
+        maxWidth: 360,
+        bgcolor: 'background.paper',
+    };
+
     return (
         <div className={classes.dialogs}>
 
-            <div className={classes.dialogsItemsAndmessagesElements}>
-                <div className={classes.dialogsItems}>
-                    {dialogsElements}
-                </div>
-                <div className={classes.messageContainer}>{messagesElements}</div>
+            {/*<div className={classes.dialogsItemsAndmessagesElements}>*/}
+            <div className={classes.dialogsElementsMessagesElements}>
+                <div>
+                    <List sx={style} component="nav" aria-label="mailbox folders">
+                        <Divider/>
+                        <ListItem button divider>
+                            <ListItemText primary={dialogsElements}/>
+                        </ListItem>
 
-                <div className={classes.messages}>
-                    <AddMessageFormRedux onSubmit={addNewMessage}/>
+                        <Divider light/>
+                    </List>
                 </div>
+
+                <div>
+                    <List sx={style} component="nav" aria-label="mailbox folders">
+                        <Divider/>
+                        <ListItem button divider>
+                            <ListItemText primary={messagesElements}/>
+                        </ListItem>
+                        <Divider light/>
+                    </List>
+                </div>
+
             </div>
 
+
+            <div className={classes.messages}>
+                <AddMessageFormRedux onSubmit={addNewMessage}/>
+            </div>
+            {/*</div>*/}
 
 
         </div>
@@ -61,7 +84,7 @@ const maxLength50 = maxLengthCreator(50)
 
 const AddMessageForm: React.FC<InjectedFormProps<FormDataAddMessageType>> = (props) => {
     return (
-        <form onSubmit={props.handleSubmit}>
+        <form onSubmit={props.handleSubmit} className={classes.messages}>
             <div>
                 <Field component={Textarea}
                        validate={[required, maxLength50]}
